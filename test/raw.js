0a11b027e0601a60d1240c7e8527b0df91107e6b
Mathieu Sylvain <mathieu@ti-coco.com>
2012-04-20 17:34:19 -0400

    Support for numeric, names, function calls

--- test/raw.js
+++ test/raw.js
@@ -6,14 +6,42 @@ var
 
 var context = {
 	lorem: "ipsum",
-	kids: {
+	kidsObj: {
 		first: "Billy",
 		second: "Julia"
 	},
+	kids: [
+		"Billy",
+		"Julia"
+	],
 	allKids: function () {
 		return this.kids.join(", ")
+	},
+	getKid: function (i) {
+		return this.kids[i]
 	}
 };
 
-funex("lorem.patate('this, is not', val[0], this)", context);
-funex("{ a:[1,2] , b:[3,4] }", context);
+var result = funex("lorem", context);
+assert.equal(result, "ipsum");
+
+var result = funex("kidsObj.first", context);
+assert.equal(result, "Billy");
+
+// Function call with no argument
+var result = funex("allKids()", context);
+assert.equal(result, "Billy, Julia");
+
+// Function call with a multiple argument
+var result = funex("getKid(1)", context);
+assert.equal(result, "Julia");
+
+/*
+// Function call with a single argument
+var result = funex("allKids()", context);
+assert.equal(result, "Billy, Julia");
+ */
+
+
+//funex("lorem.patate('this, is not', val[0], this)", context);
+//funex("{ a:[1,2] , b:[3,4] }", context);
