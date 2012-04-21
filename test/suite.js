cc79fb8d223f0fcf651be18a02ed67ee8a4378de
Mathieu Sylvain <mathieu@ti-coco.com>
2012-04-21 19:45:55 -0400

    Fixed a bug on consecutive closing instructions Added negative numbers Some more tests

--- test/suite.js
+++ test/suite.js
@@ -1,5 +1,5 @@
 var
-		cycles = 1000000,
+		cycles = 10000,
 		evalComparison = false,
 		fixtures = require("./fixtures");
 		runner = require("./runner");
@@ -8,7 +8,10 @@ var
 var tests = [
 		["Simple variable access", "lorem", "ipsum"]
 		,["Simple string", "'TEST'", "TEST"]
-		,["Simple number", "12345", 12345]
+		,["Simple numeric integer", "12345", 12345]
+		,["Simple numeric decimal", "2345.234", 2345.234]
+		,["Simple numeric decimal with zero'", "01315.2380", 1315.238]
+		,["Simple negative numeric decimal with zero'", "-01315.2380", -1315.238]
 		,["Dot notation", "kidsObj.first", "Billy"]
 		,["Dot notation with whitespace", "  kidsObj.first  ", "Billy"]
 		,["Dot notation with whitespace around the dot", "  kidsObj  .  first  ", "Billy"]
@@ -17,7 +20,15 @@ var tests = [
 		,["Array call with number", "kids[0]", "Billy"]
 		,["Array call with string", "kidsObj['second']", "Julia"]
 		,["Function call with multiple arguments", "plus3(1, 2, 3)", 6]
-		,["Function call with multiple complex arguments", "join(kidsObj.first, kidsObj.second)", "Billy-Julia"]
+		,[
+			"Function call with multiple complex arguments",
+			"join(kidsObj.first, kidsObj.second)",
+			"Billy-Julia"
+		],[
+			"Function call within function calls with multiple complex arguments",
+			"join(join(kidsObj.first, kidsObj.second), join(kidsObj.second, kidsObj.first))",
+			"Billy-Julia-Julia-Billy"
+		]
 ];
 
 runner(tests, fixtures, cycles, evalComparison);
